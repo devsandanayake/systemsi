8fd2c7388fbf5e106eb74261fd056fcede8f840d
devsandanayake <devinsandanayake49@gmail.com>
2024-06-13 11:02:44 +0530

    up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react'
-import {BrowserRouter,Route, Routes} from 'react-router-dom'
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './Components/Home'
 import Bilinsert from './Components/Bilinsert'
 import Bil from './Components/Bil'
@@ -9,26 +9,25 @@ import AISecureNet from './Components/AISecureNet/AISecureNet'
 import AINetGuard from './Components/AINetGuard/AINetGuard'
 import NetGuard from './Components/NetGuard/NetGuard'
 import Register from './Components/UserModule/Register'
-import Login from './Components/UserModule/Loging'
+import Login from './Components/UserModule/Login'
 
-export default class App extends Component {
-  render() {
-    return (
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/bilinsert" element={<Bilinsert />} />
-          <Route path="/bil" element={<Bil />} />
-          <Route path="/initiationup" element={<InitiationUp />} />
-          <Route path="/initiationgpon" element={<InitiationGPON />} />
-          <Route path="/aisecurenet" element={<AISecureNet />} />
-          <Route path="/ainetguard" element={<AINetGuard />} />
-          <Route path="/netguard" element={<NetGuard />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
-      </BrowserRouter>
-    )
-  }
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/bilinsert" element={<Bilinsert />} />
+        <Route path="/bil" element={<Bil />} />
+        <Route path="/initiationup" element={<InitiationUp />} />
+        <Route path="/initiationgpon" element={<InitiationGPON />} />
+        <Route path="/aisecurenet" element={<AISecureNet />} />
+        <Route path="/ainetguard" element={<AINetGuard />} />
+        <Route path="/netguard" element={<NetGuard />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
+export default App
